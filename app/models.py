d286dd0a7b0d27ef7a9f5ae117098f76ea6156b2
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2025-07-14 11:42:53 -0300

    feat: Post model and relationship added

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from typing import Optional
+from datetime import datetime, timezone
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -11,5 +12,18 @@ class User(db.Model):
                                              index=True, unique=True)
     password_hash : so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self) -> str:
+        return '<Post {}>'.format(self.body)
